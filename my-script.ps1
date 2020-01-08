79deee5f8ed1181220c97cbb5965b68486d1f198
Amelie Renard <amelie.renard@sonarsource.com>
2020-01-08 11:26:34 +0100

    peach

--- my-script.ps1
+++ my-script.ps1
@@ -1,2 +1,2 @@
-Invoke-WebRequest -Uri 'http://localhost:9000/static/cpp/build-wrapper-win-x86.zip' -OutFile 'build-wrapper.zip'
+Invoke-WebRequest -Uri 'https://peach.sonarsource.com/static/cpp/build-wrapper-win-x86.zip' -OutFile 'build-wrapper.zip'
 Expand-Archive -Path 'build-wrapper.zip' -DestinationPath '.'
